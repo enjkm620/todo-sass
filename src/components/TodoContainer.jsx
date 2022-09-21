4791b519fad653bc1a17f2013949a653bfd1aaaa
김은지 <enjkm62@gmail.com>
2022-09-21 11:32:39 +0900

    최초업로드

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,13 @@
+import TodoHead from "./TodoHead";
+import TodoBody from "./TodoBody";
+
+const TodoContainer = ()=>{
+    return (
+        <div className="container">
+            <TodoHead />
+            <TodoBody />
+        </div>
+    );
+}
+
+export default TodoContainer;
